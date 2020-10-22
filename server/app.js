280aaf7d3e466350d5bbbd34df00a31d73533a82
Abhishek Sisodiya <sisodiya2421@gmail.com>
2020-10-23 00:01:16 +0530

    fixed all issues with server

--- server/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.use('/', express.static('./src', {
-    index: "index.html"
-}))
-
-app.listen(port, () => console.log(`Cloth Detector listening on port ${port}!`))
