c0facef93831d17c3b95c2ea2f0313a5955b8e71
Abhishek Sisodiya <sisodiya2421@gmail.com>
2020-10-23 21:55:56 +0530

    template for the model

--- index.js
+++ index.js
@@ -4,8 +4,4 @@ const PORT = 3000;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
