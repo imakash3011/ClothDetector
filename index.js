7c321cf0254d0e73d5076245694313b9d257d9f2
Abhishek Sisodiya <sisodiya2421@gmail.com>
2020-11-02 16:07:10 +0530

    removed unnecessary lines

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const app = express();
-var cors = require('cors')
 let port = process.env.PORT;
 
 if (port == null || port == "") {
     port = 8000;
 }
 
-app.use(cors())
-
-app.use(express.static("public"), cors())
+app.use(express.static("public"))
 
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
