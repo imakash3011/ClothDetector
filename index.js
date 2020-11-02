0093f24c2d377c3f8c923410840081f85891633b
Abhishek Sisodiya <sisodiya2421@gmail.com>
2020-11-02 16:04:04 +0530

    added local model

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+var cors = require('cors')
 let port = process.env.PORT;
 
 if (port == null || port == "") {
     port = 8000;
 }
 
-app.use(express.static("public"))
+app.use(cors())
+
+app.use(express.static("public"), cors())
 
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
